7dbe34eb68dba25f32993c65ba769a6151e8dffe
LesleyM27 <llesleymabaso@gmail.com>
2024-04-20 15:29:03 +0200

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
+function updateWeather(response) {
+   let temperatureElement = document.querySelector("#temperature");
+   let temperature = response.data.temperature.current;
+   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+function searchCity(city) {
+    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateWeather);
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
-    let searchInput = document.querySelector("#search-input")
-    let cityElement = document.querySelector("#city")
-    cityElement.innerHTML = searchInput.value
+    let searchInput = document.querySelector("#search-input");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
 }
 
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit)
+searchFormElement.addEventListener("submit", handleSearchSubmit);
